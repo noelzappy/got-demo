9556b6d580afb3cf59977f4b73429e09f0662aaa
Emmanuel Yeboah <noelzappy@gmail.com>
2022-11-15 23:31:12 +0000

    updated assets url for vercel

--- src/app/theme/index.js
+++ src/app/theme/index.js
@@ -10,7 +10,7 @@ const themes = {
     primaryHover: "#d3d3d3",
     navBg: "#212529",
     danger: "#0d6efd",
-    bgImage: `url("assets/bg.jpg")`,
+    bgImage: `url("https://i.ibb.co/ZTRCcDH/bg.jpg")`,
     bgOverlay: "rgba(0, 0, 0, 0.8)",
   },
   light: {
@@ -22,7 +22,7 @@ const themes = {
     primaryHover: "#343a40",
     navBg: "#f8f9fa",
     danger: "#0d6efd",
-    bgImage: `url("assets/bg.jpg")`,
+    bgImage: `url("https://i.ibb.co/ZTRCcDH/bg.jpg")`,
     bgOverlay: "rgba(255, 255, 255, 0.4)",
   },
 };
