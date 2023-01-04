bed9788c93140996d819d251a634f2055a83cc7c
Emmanuel Yeboah <noelzappy@gmail.com>
2023-01-04 18:07:10 +0000

    updated all tests

--- src/configs/mocks/handlers.js
+++ src/configs/mocks/handlers.js
@@ -5,13 +5,6 @@ import { books, characters, houses } from "./data";
 export const handlers = [
   // Books API Handler
   rest.get(`${Config.API_URL}/books`, (req, res, ctx) => {
-    const { id } = req.params;
-
-    if ((id && Number.isNaN(id)) || id < 1 || id > books.length)
-      return res(ctx.status(404), ctx.delay(30));
-
-    if (id) return res(ctx.status(200), ctx.json(books[id - 1]), ctx.delay(30));
-
     return res(
       ctx.status(200),
       ctx.json(
@@ -25,7 +18,6 @@ export const handlers = [
   }),
 
   rest.get(`${Config.API_URL}/books/:id`, (req, res, ctx) => {
-    console.log(req);
     const { id } = req.params;
 
     if ((id && Number.isNaN(id)) || id < 1 || id > books.length)
@@ -33,28 +25,11 @@ export const handlers = [
 
     if (id) return res(ctx.status(200), ctx.json(books[id - 1]), ctx.delay(30));
 
-    return res(
-      ctx.status(200),
-      ctx.json(
-        books.map((book, index) => ({
-          ...book,
-          id: index + 1,
-        }))
-      ),
-      ctx.delay(30)
-    );
+    return res(ctx.status(404), ctx.delay(30));
   }),
 
   // Characters API Handler
   rest.get(`${Config.API_URL}/characters`, (req, res, ctx) => {
-    const { id } = req.params;
-
-    if ((id && Number.isNaN(id)) || id < 1 || id > characters.length)
-      return res(ctx.status(404), ctx.delay(30));
-
-    if (id)
-      return res(ctx.status(200), ctx.json(characters[id - 1]), ctx.delay(30));
-
     return res(
       ctx.status(200),
       ctx.json(
@@ -65,6 +40,18 @@ export const handlers = [
       ),
       ctx.delay(30)
     );
+  }),
+
+  rest.get(`${Config.API_URL}/characters/:id`, (req, res, ctx) => {
+    const { id } = req.params;
+
+    if ((id && Number.isNaN(id)) || id < 1 || id > characters.length)
+      return res(ctx.status(404), ctx.delay(30));
+
+    if (id)
+      return res(ctx.status(200), ctx.json(characters[id - 1]), ctx.delay(30));
+
+    return res(ctx.status(404), ctx.delay(30));
   }),
 
   // Houses API Handler
@@ -86,5 +73,17 @@ export const handlers = [
       ),
       ctx.delay(30)
     );
+  }),
+
+  rest.get(`${Config.API_URL}/houses/:id`, (req, res, ctx) => {
+    const { id } = req.params;
+
+    if ((id && Number.isNaN(id)) || id < 1 || id > houses.length)
+      return res(ctx.status(404), ctx.delay(30));
+
+    if (id)
+      return res(ctx.status(200), ctx.json(houses[id - 1]), ctx.delay(30));
+
+    return res(ctx.status(404), ctx.delay(30));
   }),
 ];
